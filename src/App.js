017f7932ae4a6ccb4a9970f9efb2bffe84626c58
rendratry <rendratrykusuma@gmail.com>
2023-12-18 15:22:28 +0700

    merubah favicon dan tittle

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
 import data from './/data.json';
 import './App.css';
 import LokasiTpsComponent from "./LokasiTpsConponent";
-import {useEffect, useState} from "react";
 
 function App() {
-  // const [data, setData] = useState([]);
-  //
-  // useEffect(() => {
-  //   // Fungsi untuk membaca file JSON
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await fetch('src/data.json'); // Gantilah dengan path sesuai dengan struktur proyek Anda
-  //       const jsonData = await response.json();
-  //       console.log(jsonData)
-  //       setData(jsonData); // Sesuaikan dengan struktur data JSON yang diinginkan
-  //     } catch (error) {
-  //       console.error('Error fetching data:', error);
-  //     }
-  //   };
-  //
-  //   // Panggil fungsi fetchData pada saat komponen dimuat
-  //   fetchData();
-  // }, []);
   return (
       <div>
         <LokasiTpsComponent data={data} />
